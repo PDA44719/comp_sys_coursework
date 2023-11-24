ad218315b5d8bea262199c34b8511b9176ca79f7
PDA44719 <zceepde@ucl.ac.uk>
2023-11-24 23:40:26 +0000

    Added a produce_job function for producers and tested that it works.

--- new.cpp
+++ new.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<queue>
 #include<string>
+#include<cstdlib>
+#include<ctime>
 
 using namespace std;
 
@@ -21,15 +23,33 @@ void parseArgument(int index, char* arguments[], int* params){
 	}
 }
 
+void produce_job(int* queue_ptr, int size){
+	srand((int) time(0)); // Generate a seed
+	for (int i=0; i<size; i++){
+		if (queue_ptr[i] == 0){
+			queue_ptr[i] = rand()%10 + 1;
+			return;
+		}
+	}
+}
+
+void process_job(int* queue){
+
+}
+
 int main(int argc, char* argv[]){
-	cout << "The arguments entered were: ";
 	int parameters[4];
 	for (int i=0; i<argc; i++){
 		if (argv[i][0] == '-')
 			parseArgument(i, argv, &parameters[0]);
 	}
-	for (int j=0; j<4; j++){
-		cout << parameters[j] << endl;
-	}
+	int queue_size = parameters[0];
+	int queue[100] = {0}; // 100 would be the max size of the buffer
+	produce_job(queue, queue_size);
+	cout << "First job finished" << endl;
+	cout << "Second job starting" << endl;
+	produce_job(queue, queue_size);
+	for (int i=0; i<queue_size; i++)
+		cout << queue[i] << endl;
 	return 0;
 }
